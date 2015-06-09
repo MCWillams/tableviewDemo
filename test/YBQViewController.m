293d723e602f2e201dd5903732341914088df757
MCWillams <yanbiqing@yanbiqingdeMac.local>
2015-06-08 23:34:48 -0700

    tableview

--- test/YBQViewController.m
+++ test/YBQViewController.m
@@ -8,22 +8,39 @@
 
 #import "YBQViewController.h"
 
-@interface YBQViewController ()
-
+@interface YBQViewController ()<UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic,strong) NSArray *names;
 @end
 
 @implementation YBQViewController
 
 - (void)viewDidLoad
 {
+
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.tableView.dataSource = self;
+}
+/**
+ *  懒加载（用到时再加载）使用点语法时才调用
+ *
+ */
+-(NSArray *)names{
+    if (_names == nil) {
+        _names = @[@"yan",@"lbj",@"jr",@"kobe"];
+    }
+    return _names;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSLog(@"number");
+    return self.names.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"cell");
+    UITableViewCell *cell = [[UITableViewCell alloc]init];
+    cell.textLabel.text = self.names[indexPath.row];
+    return cell;
+}
 @end
